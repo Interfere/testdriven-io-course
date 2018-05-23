c58d8ab2ed8d6af6c06702420f74324009328126
Interfere <interfere.work@gmail.com>
2018-05-23 20:20:02 +0300

    Postgres

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,17 +1,26 @@
 # services/users/project/config.py
 
+
+import os
+
+
 class BaseConfig:
-	"""Base configuration"""
-	TESTING = False
+    """Base configuration"""
+    TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 class DevelopmentConfig(BaseConfig):
-	"""Development configuration"""
-	pass
+    """Development configuration"""
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
 
 class TestingConfig(BaseConfig):
-	"""Testing configuration"""
-	TESTING = True
+    """Testing configuration"""
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
+
 
 class ProductionConfig(BaseConfig):
-	"""Production configuration"""
-	pass
+    """Production configuration"""
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
